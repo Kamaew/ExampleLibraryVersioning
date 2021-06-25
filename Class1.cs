4ad564f87e569eacf5242c99ec36640cdf9a92fa
Максим <kaverin.m@outlook.com>
2021-06-25 08:08:49 +0600

    Добавил манифест для 7.3

--- Class1.cs
+++ Class1.cs
@@ -15,7 +15,7 @@ namespace ExampleLibraryVersioning
     public class Class1 : IFrontPlugin
     {
         private readonly Stack<IDisposable> subscriptions = new Stack<IDisposable>();
-        Class1()
+        public Class1()
         {
             subscriptions.Push(new AddOneButton());
             subscriptions.Push(PluginContext.Notifications.SubscribeOnBeforeOrderBill(OnNavigating));
