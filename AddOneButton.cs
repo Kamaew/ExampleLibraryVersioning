fcf6d9e8175a09e12a8742abce730b8ed12b255d
Максим <kaverin.m@outlook.com>
2021-06-25 07:56:48 +0600

    Плагин для версии iiko 7.3.6

---
+++ AddOneButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive.Disposables;
+using Resto.Front.Api.Attributes.JetBrains;
+using Resto.Front.Api;
+using Resto.Front.Api.UI;
+using Resto.Front.Api.Extensions;
+
+namespace ExampleLibraryVersioning
+{
+    public class AddOneButton : IDisposable
+    {
+        [CanBeNull]
+        private readonly CompositeDisposable subscriptions;
+        public AddOneButton()
+        {
+            subscriptions = new CompositeDisposable
+            {
+               PluginContext.Integration.AddButton("Тестовая кнопка",DownButton)
+            };
+        }
+
+        private void DownButton(IViewManager viewManager, IReceiptPrinter receiptPrinter)
+        {
+            PluginContext.Log.Info("На кнопку нажали.");
+        }
+        public void Dispose()
+        {
+            subscriptions.Dispose();
+        }
+    }
+}
--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Resto.Front.Api;
+using Resto.Front.Api.Attributes;
+using Resto.Front.Api.Data.Orders;
+using Resto.Front.Api.Extensions;
+using Resto.Front.Api.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +11,24 @@ using System.Threading.Tasks;
 
 namespace ExampleLibraryVersioning
 {
-    public class Class1
+    [PluginLicenseModuleId(21016318)]
+    public class Class1 : IFrontPlugin
     {
+        private readonly Stack<IDisposable> subscriptions = new Stack<IDisposable>();
+        Class1()
+        {
+            subscriptions.Push(new AddOneButton());
+            subscriptions.Push(PluginContext.Notifications.SubscribeOnBeforeOrderBill(OnNavigating));
+        }
 
+        private void OnNavigating(IOrder arg1, IViewManager arg2)
+        {
+            PluginContext.Log.Info($"Оплата заказа {arg1.Id}");
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
